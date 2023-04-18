cac4f67568e9e8fbcf96259371180946ede644dc
Vivatey <vivatey@gmail.com>
2023-04-18 02:49:48 +0100

    Added some styling and styling components.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import Dictionary from "./Dictionary";
 import "./App.css";
 
-
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <i className="fa-solid fa-spell-check fa-2xl App-logo"></i>
-          <h1>Dictionary</h1>
+          <h1>
+            <i className="fa-solid fa-spell-check fa-2xl App-logo"></i>
+            Dictionary
+          </h1>
         </header>
         <main>
           <Dictionary />
         </main>
-        <footer className="App-footer"><small>Coded by Viv and is Open-sourced</small></footer>
+        <footer className="App-footer">
+          <small>
+            Coded by Viv and is
+            <a
+              href="https://github.com/Vivatey/dictionary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Open-sourced
+            </a>
+          </small>
+        </footer>
       </div>
     </div>
   );
